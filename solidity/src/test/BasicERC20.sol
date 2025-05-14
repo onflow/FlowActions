bdd62f44aafab8a51e63a539d219ead53e15b29c
Giovanni Sanchez <108043524+sisyphusSmiling@users.noreply.github.com>
2025-05-14 09:05:25 -0600

    fix solidity formatting

--- solidity/src/test/BasicERC20.sol
+++ solidity/src/test/BasicERC20.sol
@@ -7,10 +7,11 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract BasicERC20 is ERC20, ERC20Burnable, Ownable, ERC20Permit {
-    constructor(
-        string memory name_,
-        string memory symbol_
-    ) ERC20(name_, symbol_) Ownable(msg.sender) ERC20Permit(name_) {}
+    constructor(string memory name_, string memory symbol_)
+        ERC20(name_, symbol_)
+        Ownable(msg.sender)
+        ERC20Permit(name_)
+    {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
